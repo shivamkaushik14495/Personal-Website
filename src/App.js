f5cc1ed31a9e83e15fd63cdd4ad2eadea0809728
Shivam Kaushik <shivamkaushik14495@gmail.comm>
2022-08-14 23:42:10 +0530

    added Linkes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import profile from "./image/image.png"
 function App() {
   return (
     <div className="App">
-    <nav className='bg-black h-16'>
-      <ul className='flex float-right mx-10 mt-6 text-center'>
-        <li><a href='#' className='pd mx-4 px-4 text-white'>Behance</a></li>
-        <li><a href='#' className='pd mx-4 px-4 text-white'>Dribbble</a></li>
-        <li><a href='#' className='pd mx-4 px-4 text-white'>Linkedin</a></li>
+    <nav className='bg-white sm:bg-black h-16'>
+      <ul className='flex justify-between sm:float-right mx-10 mt-6 text-center'>
+        <li><a href='https://www.behance.net/shivamkaushik1' className=' text-black font-bold px-0 mx-0 sm:text-white sm:mx-4 sm:px-4'>Behance</a></li>
+        <li><a href='https://dribbble.com/Shivamkaushik' className='text-black font-bold px-0 mx-0 sm:text-white sm:mx-4 sm:px-4 '>Dribbble</a></li>
+        <li><a href='https://www.linkedin.com/in/shivam-kaushik-b8340114b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BosKtHSFqSr%2BqjtfvOL1KEQ%3D%3D' className='text-black font-bold px-0 mx-0 sm:text-white sm:mx-4 sm:px-4 '>Linkedin</a></li>
       </ul>
     </nav>
     <div className='body block mt-0 py-10 sm:grid text-left min-h-full grid-cols-2 justify-items-center items-center sm:mt-28'>
     <>
-      <img src={profile} className= "2xl:max-w-xl xl:max-w-lg float-right lg:max-w-sm sm:max-w-xs"></img>
+      <img src={profile} className= "2xl:max-w-xl xl:max-w-lg float-right lg:max-w-sm sm:max-w-xs" alt='profile'></img>
     </>
     <div className=' text-center sm:text-left'>
       <h3 className='heading text-4xl 2xl:text-8xl xl:text-7xl lg:text-5xl'>Hello! I'm</h3>
